25a7bf3eaf526ad0b85f97dd753e5118152663ad
screwthecap1 <chanskiy777@mail.ru>
2025-09-01 17:02:48 +0400

    init: base Laravel 12 setup (+ auth, sanctum, queues, roles)

---
+++ app/Http/Middleware/EnsureAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || $request->user()->role !== Role::ADMIN) {
+            abort(404);
+        }
+        return $next($request);
+    }
+}
